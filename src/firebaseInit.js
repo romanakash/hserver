98da1df1fb86b7dc9801426be2be1e0b7842ec15
romanakash <romanakash2000@gmail.com>
2020-02-05 21:27:30 +0000

    process env

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -2,9 +2,9 @@ const firebaseAdmin = require('firebase-admin');
 
 firebaseAdmin.initializeApp({
 	credential: firebaseAdmin.credential.cert({
-		projectId: GCS_PROJECT_ID,
-		clientEmail: GCS_CLIENT_EMAIL,
-		privateKey: GCS_PRIVATE_KEY
+		projectId: process.env.GCS_PROJECT_ID,
+		clientEmail: process.env.GCS_CLIENT_EMAIL,
+		privateKey: process.env.GCS_PRIVATE_KEY
 	}),
 	databaseURL: 'https://created-2020.firebaseio.com'
 });
