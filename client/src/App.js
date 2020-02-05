89502e40017952e518d1fd19b65eb3cda2dcd9d0
romanakash <romanakash2000@gmail.com>
2020-02-05 18:21:00 +0000

    basic form

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'emotion-theming';
+import theme from '@rebass/preset';
+import { Box, Flex, Button } from 'rebass';
+import { Label, Input, Select, Textarea, Radio, Checkbox } from '@rebass/forms';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const handleSubmit = event => {
+		event.preventDefault();
+		const formData = {
+			field: event.target[0].value
+		};
+		const userData = { formData };
+
+		console.log(userData);
+	};
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Box as="form" py={3} onSubmit={handleSubmit}>
+				<Flex mx={-2} mb={3}>
+					<Box width={1 / 2} px={2}>
+						<Label htmlFor="field">Field</Label>
+						<Input
+							id="field"
+							name="field"
+							defaultValue="Jane Doe"
+						/>
+					</Box>
+				</Flex>
+				<Box px={2} ml="auto">
+					<Button type="submit" variant={'primary'}>
+						Submit
+					</Button>
+				</Box>
+			</Box>
+		</ThemeProvider>
+	);
 }
 
 export default App;
