eb8456ca7bee22ce06e885dee530c058d496f160
romanakash <romanakash2000@gmail.com>
2020-02-06 22:46:06 +0000

    cors

--- src/server.js
+++ src/server.js
@@ -9,14 +9,7 @@ const { getMLHUserData } = require('./mlhApi');
 const { getFormData } = require('./getFormData');
 const { modifyUser } = require('./modifyUser');
 
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
-	);
-	next();
-});
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
