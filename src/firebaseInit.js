844b75a2bdc1b59adcc3e5736296f4c5830052ec
romanakash <romanakash2000@gmail.com>
2020-02-06 13:23:20 +0000

    Fixed private key

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -3,8 +3,8 @@ const firebaseAdmin = require('firebase-admin');
 firebaseAdmin.initializeApp({
 	credential: firebaseAdmin.credential.cert({
 		projectId: process.env.GCS_PROJECT_ID,
-		clientEmail: process.env.GCS_CLIENT_EMAIL
-		//privateKey: process.env.GCS_PRIVATE_KEY.replace(/\\n/g, '\n')
+		clientEmail: process.env.GCS_CLIENT_EMAIL,
+		privateKey: process.env.GCS_PRIVATE_KEY.replace(/\\n/g, '\n')
 	}),
 	databaseURL: 'https://created-2020.firebaseio.com'
 });
