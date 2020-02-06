ba6fe85cefea87db13f1ff78a15f35f31dae89fe
romanakash <romanakash2000@gmail.com>
2020-02-06 23:32:44 +0000

    cors5

--- src/server.js
+++ src/server.js
@@ -9,8 +9,14 @@ const { getMLHUserData } = require('./mlhApi');
 const { getFormData } = require('./getFormData');
 const { modifyUser } = require('./modifyUser');
 
-app.use(cors());
-app.options('*', cors());
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
 
 app.use((req, res, next) => {
 	const token = req.get('authorization');
