7d2506d6ad33980e62f9e3684072d0e054d0e661
romanakash <romanakash2000@gmail.com>
2020-02-06 23:26:16 +0000

    cors1

--- src/server.js
+++ src/server.js
@@ -9,14 +9,7 @@ const { getMLHUserData } = require('./mlhApi');
 const { getFormData } = require('./getFormData');
 const { modifyUser } = require('./modifyUser');
 
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.writeHead(200);
-	next();
-});
-
-app.use(express.static(path.join(__dirname, 'build')));
-app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
 	const token = req.get('authorization');
