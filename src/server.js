a74a015c706779a0b54f3f436885fafb0013d0cd
romanakash <romanakash2000@gmail.com>
2020-02-06 22:41:43 +0000

    custom cors headers

--- src/server.js
+++ src/server.js
@@ -9,7 +9,14 @@ const { getMLHUserData } = require('./mlhApi');
 const { getFormData } = require('./getFormData');
 const { modifyUser } = require('./modifyUser');
 
-app.use(cors());
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
